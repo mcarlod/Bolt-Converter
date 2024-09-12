63b91aa3ece3448b7bc9bd2fe296850081e47571
mcarlod <carlo.diesta@gmail.com>
2024-09-12 16:46:33 +0930

    Created the base layout of the bolt size converter app and included necessary elements that would be necessary and as well as starting to display the image of a bolt size converter chart.

--- Bolt Converter/ViewController.swift
+++ Bolt Converter/ViewController.swift
@@ -11,9 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let imageUrl = URL(string: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.fixingsmegastore.co.uk%2Fhelp%2Fmetric-imperial-conversion-table&psig=AOvVaw0gWAgL4MKPtyUFLg3YY355&ust=1726211300410000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJDKwK3svIgDFQAAAAAdAAAAABAE") else {
+            print("Invalid URL")
+            return
+        }
     }
 
-
+    @IBOutlet weak var boltChartImage: UIImageView!
+    
+    
 }
 
